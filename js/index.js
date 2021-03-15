60dc47cc4b8794b47083f6f0a8ae0726c869eeea
Sarah Guidry <sarahbethguidry@gmail.com>
2021-03-15 05:40:09 -0500

    took out listener method

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 import { gsap } from "gsap";
 gsap.registerPlugin(ScrollTrigger);
 
-function listener(event) {
-    console.log(`
-        event passing through ${event.currentTarget.nodeName || 'Window'}
-        target-->${event.target.nodeName}`);
-}
 //**********************#1 & #2********************************************************************** */
 // add mouseover and mouseout event listeners
 let navLinks = document.querySelectorAll(".nav-link");
@@ -58,18 +53,15 @@ btns.forEach((btn) => {
 });
 let btn0 = document.querySelector('#btn0');
 btn0.addEventListener('dblclick', (event) => {
-    listener(event);
     dblClickBtn(btn0);
 });
 
 let btn1 = document.querySelector('#btn1');
 btn1.addEventListener('dblclick', (event) => {
-    listener(event);
     dblClickBtn(btn1);
 });
 let btn2 = document.querySelector("#btn2");
 btn2.addEventListener('dblclick', (event) => {
-    listener(event);
     dblClickBtn(btn2);
 });
 
@@ -102,7 +94,6 @@ letsGoImg.style.transition = 'all 2s ease-in-out'; //sets transition parameters
 newImg.style.width = "400px";
 let mouseDownEvent = document.addEventListener('mousedown', (event) => { // create event listener
     // add css for crossfading 
-    listener(event);
     letsGoImg.style.opacity = 0;
     newImg.style.display = 'block';
     newImg.style.opacity = 1;
@@ -113,7 +104,6 @@ let mouseDownEvent = document.addEventListener('mousedown', (event) => { // crea
 let text = document.querySelectorAll('p'); // select all paragraphs
 text.forEach((txt) => { // adding contextmenu event listener to each
     txt = addEventListener('contextmenu', e => {
-        listener(e);
         e.preventDefault(); // preventing the contextmenu from showing when right clicking
     });
 });
@@ -123,7 +113,6 @@ text.forEach((txt) => { // adding contextmenu event listener to each
 let headers = document.querySelectorAll('h2');
 headers.forEach((header) => {
     header.addEventListener('mouseup', (event) => {
-        listener(event);
         header.style.transform = 'scale(1.5) translate(-50%, -50%)';
         stopPropagation(event);
     });
@@ -132,7 +121,6 @@ headers.forEach((header) => {
 //****************#8********************************************************************************* */
 // add keydown event listener
 document.body.addEventListener('keydown', (event) => {
-    listener(event);
     pageHeader.style.animation = '3s linear 1s infinite running slidein';
 });
 
@@ -146,15 +134,12 @@ document.addEventListener('scroll', (event) => {
         scrollTrigger: destination,
         x: 500
     });
-    // ScrollTrigger.create({
-    //     trigger:'#'
-    // })
+
 });
 //**************#10********************************************************************************* */
 // add keyup event listener
 
 document.body.addEventListener("keyup", (event) => {
-    listener(event);
     alert(event);
     stopPropagation(event);
 });
